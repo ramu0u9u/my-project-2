a0b74a8d39340ed9375175f61c3ef0da9010dbc0
dda <99751793+ramu0u9u@users.noreply.github.com>
2024-06-25 13:08:10 +0530

    Update IndexController.java

--- src/main/java/com/edw/controller/IndexController.java
+++ src/main/java/com/edw/controller/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 
         return new HashMap<>() {{
             put("success", true);
-            put("hello", "worldd");
+            put("hello", "world");
             put("new-message", "adding a new msg");
         }};
     }
